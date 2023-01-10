09f2d0d384f79e0003b761b3cf093bcb0d2ee63e
itznakru <antongrishko@gmail.com>
2023-01-10 21:35:22 +0400

    remove DevApp

--- DevApp/src/AppSettings.js
+++
@@ -1,5 +0,0 @@
-const appSettings={
-    REST_SERVERURL:"http://localhost:5141/api/",
-    ERROR_PAGE:"/error"
-}
-export {appSettings}
--- DevApp/src/tools-middleware/log-mdl.js
+++
@@ -1,5 +0,0 @@
-export const logMdl=store=>next=>action=>{
-    const result =next(action);
-    console.log("next state:",store.getState())
-    return result;
-};
